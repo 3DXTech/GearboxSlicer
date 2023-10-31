8d9c116ae45133695e8e64ab3dd2045c296cdd22
ericedson98 <121807834+ericedson98@users.noreply.github.com>
2023-10-31 14:15:18 -0400

    VolumetricWipe is no longer limited 4000

--- installer/GearboxInstaller/cura-files/programfiles/plugins/PostProcessingPlugin/scripts/VolumetricWipe.py
+++ installer/GearboxInstaller/cura-files/programfiles/plugins/PostProcessingPlugin/scripts/VolumetricWipe.py
@@ -36,7 +36,7 @@ class VolumetricWipe(Script):
                     "label": "Extrusion Amount",
                     "description": "The amount of extrusion needed to wipe. Ex: 100 will wipe every 100mm of extrusion",
                     "type": "int",
-                    "maximum_value_warning": "5000",
+                    "maximum_value_warning": "20000",
                     "default_value": 400
                 }
             }
@@ -47,7 +47,12 @@ class VolumetricWipe(Script):
         currentTool = 0
 
         line_set = {}
-        last_rounded_e_value = 0
+        t0_total_e_value = 0
+        t1_total_e_value = 0
+        t0_last_e_value = 0
+        t1_last_e_value = 0
+        t0_last_rounded_e_value = 0
+        t1_last_rounded_e_value = 0
         for layer in data:
             line_set = {}
             layer_index = data.index(layer)
@@ -67,10 +72,24 @@ class VolumetricWipe(Script):
                     searchE = re.search(r"E([-+]?\d*\.?\d*)", line)
                     if searchE:
                         e_value=float(searchE.group(1))
-                        rounded_e = int(math.floor(e_value / self.getSettingValueByKey("extrusion"))) * self.getSettingValueByKey("extrusion")
-                        if (rounded_e != last_rounded_e_value and rounded_e > last_rounded_e_value and rounded_e >= 0 and last_rounded_e_value >= 0):
-                            lineIndex = lines.index(line)
-                            lines.insert(lineIndex, "G12")
-                        last_rounded_e_value = rounded_e
+
+                        if (currentTool == 0):
+                            if (e_value < t0_last_e_value):
+                                t0_total_e_value += t0_last_e_value
+                            rounded_e = int(math.floor((t0_total_e_value + e_value) / self.getSettingValueByKey("extrusion"))) * self.getSettingValueByKey("extrusion")
+                            if (rounded_e != t0_last_rounded_e_value and rounded_e > t0_last_rounded_e_value and rounded_e >= 0 and t0_last_rounded_e_value >= 0):
+                                lineIndex = lines.index(line)
+                                lines.insert(lineIndex, "G12 ;{}".format(t0_total_e_value + e_value))
+                            t0_last_e_value = e_value
+                            t0_last_rounded_e_value = rounded_e
+                        if (currentTool == 1):
+                            if (e_value < t1_last_e_value):
+                                t1_total_e_value += t1_last_e_value
+                            rounded_e = int(math.floor((t1_total_e_value + e_value) / self.getSettingValueByKey("extrusion"))) * self.getSettingValueByKey("extrusion")
+                            if (rounded_e != t1_last_rounded_e_value and rounded_e > t1_last_rounded_e_value and rounded_e >= 0 and t1_last_rounded_e_value >= 0):
+                                lineIndex = lines.index(line)
+                                lines.insert(lineIndex, "G12 ;{}".format(t0_total_e_value + e_value))
+                            t1_last_e_value = e_value
+                            t1_last_rounded_e_value = rounded_e
             data[layer_index] = "\n".join(lines)
         return data
